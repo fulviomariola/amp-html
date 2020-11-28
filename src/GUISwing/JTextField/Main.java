6755043ee179370891231b3b1b57ae7dc9c98415
Fulvio Mariola <fulvio.mariola@gmail.com>
2020-11-28 22:17:32 +0100

    New GUI

--- src/GUISwing/JTextField/Main.java
+++ src/GUISwing/JTextField/Main.java
@@ -1,5 +1,12 @@
 package GUISwing.JTextField;
 
 public class Main {
-
+	public static void main(String [] args) {
+		
+		// JTextField = è una GUI per i testbox può essere usato per add, 
+		//set o get text
+		
+		new MyFrame();
+		
+	}
 }
